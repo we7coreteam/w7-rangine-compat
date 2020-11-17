ae6787c271285400f08672709294a729a5b63b2d
titrxw <1120309488@qq.com>
2020-11-17 11:54:43 +0800

    添加废弃interface

---
+++ src/Core/Listener/ListenerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace W7\Core\Listener;
+
+/**
+ * @deprecated
+ * Interface ListenerInterface
+ * @package W7\Core\Listener
+ */
+interface ListenerInterface extends \W7\Contract\Listener\ListenerInterface {
+
+}
